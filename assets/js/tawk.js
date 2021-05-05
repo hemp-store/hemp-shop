a277e8c12f0994b1f2a16452dc99d74ef1cc088e
seo-serv <seoserv11@gmail.com>
2021-05-05 03:07:53 -0700

    Auto-generated commit

---
+++ assets/js/tawk.js
@@ -0,0 +1,9 @@
+var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+(function () {
+        var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+        s1.async = true;
+        s1.src='https://embed.tawk.to/5eab7823203e206707f880dc/default';
+        s1.charset = 'UTF-8';
+        s1.setAttribute('crossorigin', '*');
+        s0.parentNode.insertBefore(s1, s0);
+})();
